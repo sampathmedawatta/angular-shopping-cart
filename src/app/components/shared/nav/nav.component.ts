eb30d2a8c7807978e5410e74e087e86d093eb4c8
SamVPS <sampath.m@virtualplanet.com.au>
2020-07-03 02:02:03 +0530

    show cart itme qty in header link

--- src/app/components/shared/nav/nav.component.ts
+++ src/app/components/shared/nav/nav.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { MessengerService } from 'src/app/services/messenger.service';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-
-  constructor() { }
+  cartItems = [];
+  constructor(
+    private messengerService: MessengerService,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
+    this.messengerService
+      .getMsgAddProductToCart()
+      .subscribe((product: Product) => {
+        this.cartItems = this.cartService.getCartItems();
+      });
+    this.cartItems = this.cartService.getCartItems();
   }
-
 }
