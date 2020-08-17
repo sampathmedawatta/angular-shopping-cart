beec1fb6aa9dd4c5f1afc1159b8c9fd3bf40036f
SamVPS <sampath.m@virtualplanet.com.au>
2020-08-17 21:20:14 +0530

    set up refresh token

--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -12,6 +12,7 @@ export const categoryUrl = baseUrl + '/Category';
 export const loginUrl = baseUrl + '/User/Login';
 export const registerUrl = baseUrl + '/User/Register';
 export const userDetailsUrl = baseUrl + '/User/Profile';
+export const refreshTokenUrl = baseUrl + '/User/RefreshToken';
 
 export const orderUrl = baseUrl + '/Order';
 export const orderByIdUrl = baseUrl + '/Order';
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Auth } from '../models/auth';
 import { HttpClient } from '@angular/common/http';
-import { loginUrl, registerUrl, userDetailsUrl } from '../config/api';
+import {
+  loginUrl,
+  registerUrl,
+  userDetailsUrl,
+  refreshTokenUrl,
+} from '../config/api';
 import { Observable } from 'rxjs';
 import { Register } from '../models/register';
 import { OperationResult } from '../models/operation-result';
+import { Router } from '@angular/router';
+import { MessengerService } from './messenger.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   headers = { 'content-type': 'application/json' };
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private messengerService: MessengerService
+  ) {}
 
   login(auth: Auth): Observable<OperationResult> {
     const body = JSON.stringify(auth);
@@ -30,5 +41,24 @@ export class AuthService {
 
   getUserProfile(): Observable<any> {
     return this.http.get<any>(userDetailsUrl);
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('refreshToken');
+    localStorage.removeItem('user');
+
+    this.messengerService.sendMsgUserLogout();
+    this.router.navigateByUrl('/login');
+  }
+
+  getNewAccessToken(): Observable<any> {
+    return this.http.get<any>(refreshTokenUrl, {
+      headers: {
+        'content-type': 'application/json',
+        jwtToken: localStorage.getItem('token'),
+        refreshToken: localStorage.getItem('refreshToken'),
+      },
+    });
   }
 }
